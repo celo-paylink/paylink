2a6ba2f61a242e55b727b8a4c84963c3bf0f1a1c
Okeke Nmesomachukwu Henry <Iamhenryokeke@gmail.com>
2025-10-14 23:36:55 +0100

    feat: updated axios baseUrl

--- paylink_frontend/src/services/api.ts
+++ paylink_frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { disconnect } from '@wagmi/core'
 import { config } from "../libs/config";
 
-const baseURL = 'http://localhost:5000/api';
+const baseURL = 'https://paylink-backend-9ks2.onrender.com/api';
 
 const axiosInstance = axios.create({
   baseURL, 
