35e58a634c225e84d3ac83d609c286e13b522ded
soomtochukwu <onwuajuesesomtochukwu@gmail.com>
2025-11-26 16:43:04 +0100

    .

--- paylink_frontend/src/libs/config.ts
+++ paylink_frontend/src/libs/config.ts
@@ -1,10 +1,16 @@
 import '@rainbow-me/rainbowkit/styles.css'
 import { getDefaultConfig } from '@rainbow-me/rainbowkit'
+import {
+  injectedWallet,
+  metaMaskWallet,
+  coinbaseWallet,
+  walletConnectWallet,
+} from '@rainbow-me/rainbowkit/wallets';
 import { http } from 'viem';
 import { celo } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
-  appName: 'Minth',
+  appName: 'Paylink',
   projectId: "7b20fbb4bba28015e2d4ddfbe5d08a43",
   chains: [
     celo
@@ -12,5 +18,16 @@ export const config = getDefaultConfig({
   transports: {
     [celo.id]: http(),
   },
+  wallets: [
+    {
+      groupName: 'Recommended',
+      wallets: [
+        injectedWallet,
+        metaMaskWallet,
+        coinbaseWallet,
+        walletConnectWallet,
+      ],
+    },
+  ],
   ssr: true,
 })
